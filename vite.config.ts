7c60873ebd5a7ff49a391502bc5c9d0389a2b367
范泽辰 <1175883207@qq.com>
2025-10-02 10:22:02 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // 读取当前模式的环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_NODE_CODE == 'develop' ? '/' : '/product-web/',
+    build: {
+      outDir: 'dist',
     },
-  },
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
 })
